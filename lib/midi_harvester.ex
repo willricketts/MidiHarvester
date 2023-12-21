1b35bdecac53060bfb89cd83d22e9f66444ddaa6
Will Ricketts <will@willricketts.com>
2023-12-21 09:02:16 -0800

    handling most systems and organizing output

--- lib/midi_harvester.ex
+++ lib/midi_harvester.ex
@@ -3,31 +3,76 @@ defmodule MidiHarvester do
   Documentation for `MidiHarvester`.
   """
 
-  @target "https://www.vgmusic.com/music/console/nintendo/gameboy/"
+  require Logger
+
+  @platform_map %{
+    nintendo: [
+      "nes",
+      "gameboy",
+      "snes",
+      "n64",
+      "virtualboy",
+      "gba",
+      "gamecube",
+      "ds",
+      "3ds",
+      "wii",
+      "wiiu",
+      "switch"
+    ],
+    sega: ["master", "genesis", "gamegear", "saturn", "dreamcast", "segacd", "32x"],
+    sony: ["ps1", "ps2", "ps3", "ps4", "psp"],
+    microsoft: ["xbox", "xbox360"]
+  }
+  @base_url "https://www.vgmusic.com/music/console"
 
   def run do
-    parsed =
-      @target
-      |> HTTPoison.get!()
-      |> Map.get(:body)
-      |> Floki.parse_document!()
+    File.rm_rf!("./files")
+    File.mkdir_p!("./files")
 
-    parsed
+    @platform_map
+    |> Map.keys()
+    |> Enum.map(fn x -> harvest(x) end)
+  end
+
+  defp harvest(platform) do
+    Logger.info("#### Harvesting #{platform} midi files ####")
+
+    File.mkdir_p!("./files/#{platform}")
+
+    Enum.each(@platform_map[platform], fn x ->
+      harvest_platform(platform, x)
+    end)
+  end
+
+  defp harvest_platform(platform, console) do
+    Logger.info("$$$$ Starting harvest for #{console} $$$$")
+
+    File.mkdir_p!("./files/#{platform}/#{console}")
+
+    target = "#{@base_url}/#{platform}/#{console}/"
+
+    do_harvest(target, platform, console)
+  end
+
+  def do_harvest(target_url, platform, console) do
+    target_url
+    |> HTTPoison.get!()
+    |> Map.get(:body)
+    |> Floki.parse_document!()
     |> Floki.find("a")
     |> Enum.map(fn x -> Floki.attribute(x, "href") end)
     |> List.flatten()
     |> Enum.filter(fn x -> String.contains?(x, ".mid") end)
-    |> Enum.map(fn x -> download_file(x) end)
-
-    :ok
+    |> Enum.map(fn x -> download_file(platform, console, x) end)
   end
 
-  defp download_file(file_name) do
+  defp download_file(platform, console, file_name) do
     IO.puts("Downloading #{file_name}")
 
-    url = "#{@target}#{file_name}"
+    url = "#{@base_url}/#{platform}/#{console}/#{file_name}"
     file = HTTPoison.get!(url)
 
-    File.write!("./files/#{file_name}", file.body)
+    File.write!("./files/#{platform}/#{console}/#{file_name}", file.body)
   end
 end
